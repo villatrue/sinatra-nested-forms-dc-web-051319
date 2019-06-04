125d4badb8f1b7807ea1c06877cd13cb9e8f7e29
villatrue <scarter0313@gmail.com>
2019-06-04 15:31:37 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@pirates = []
+  def initialize(args)
+   @name = args[:name]
+   @weight = args[:weight]
+   @height = args[:height]
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
 end
